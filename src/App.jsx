18709335be70e0d42757d025e4718522c568bf72
ilariamotta <ilaria.motta93@gmail.com>
2025-12-02 16:21:55 +0100

    feat: funzione filtra per genere

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 
@@ -13,19 +14,28 @@ const filmList = [
 ]
 
 function App() {
+  const [choose, setChoose] = useState("");
+  const [filteredArray, setFilteredArray] = useState(filmList)
+
+  
+
+  useEffect(() => {
+    const newFilteredArray = filmList.filter((film) => film.genre.includes(choose));
+    setFilteredArray(newFilteredArray);
+  }, [choose])
 
   return (<div className="container">
     <div className="row">
       <div className="col">
         <div className="form-floating">
-          <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
-            <option selected>Apri la lista dei generi</option>
-            {filmList.map((film, index) => (<option value="1" key={index}>{film.genre}</option>))}
+          <select className="form-select" id="floatingSelect" aria-label="Floating label select example" value={choose} onChange={(event) => setChoose(event.target.value)}>
+            <option value="">Apri la lista dei generi</option>
+            {filmList.map((film, index) => (<option key={index} value={film.genre}>{film.genre}</option>))}
           </select>
-          <label for="floatingSelect">Seleziona il genere</label>
+          <label htmlFor="floatingSelect">Seleziona il genere</label>
         </div>
         <ul className='list-group'>
-          {filmList.map((film, index) => (<li className="list-group-item" key={index}>{film.title}</li>
+          {filteredArray.map((film, index) => (<li className="list-group-item" key={index}>{film.title}</li>
           ))}
         </ul>
       </div>
