7a35b40d59e6020d6808e0044b8ca5af6a785d37
ilariamotta <ilaria.motta93@gmail.com>
2025-12-02 15:22:50 +0100

    feat: stampa select con genere

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,29 @@ const filmList = [
 function App() {
 
   return (<div className="container">
-    <ul className='list-group'>
-      {filmList.map((film, index) => (<li className="list-group-item" key={index}>{film.title}</li>
-      ))}
-    </ul>
+    <div className="row">
+      <div className="col">
+        <div className="form-floating">
+          <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
+            <option selected>Apri la lista dei generi</option>
+            {filmList.map((film, index) => (<option value="1" key={index}>{film.genre}</option>))}
+          </select>
+          <label for="floatingSelect">Seleziona il genere</label>
+        </div>
+        <ul className='list-group'>
+          {filmList.map((film, index) => (<li className="list-group-item" key={index}>{film.title}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   </div>
   )
 }
 
+
+
 export default App
+
+
+
+
