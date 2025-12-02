f38a2fc662372585892188fa81067860c0f3e0cc
ilariamotta <ilaria.motta93@gmail.com>
2025-12-02 15:03:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+
+
+const filmList = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+function App() {
+
+  return 
+}
+
+export default App
